28c07096c42c5b74f4079fab2e19209ecd8f9d3b
Josef <josef.holmner@gmail.com>
2023-10-28 23:25:12 +0200

    Add pigpio as a dependency

--- README.md
+++ README.md
@@ -15,8 +15,7 @@ New material will be uploded as the project progresses. One goal is to provide a
 
 ## Raspberry Pi setup
 1. Install CMake (sudo apt-get install cmake).
-
-### Build and run the Raspberry Pi application Holmner6Axis
+2. Ensure pigpio is installed (included with Raspberry Pi OS).
 
 Clone this repo to a Raspberry Pi.
 To build, run the following from the repo root:
@@ -32,5 +31,5 @@ sudo cmake --build . --config Release --target install
 Now the application can be started by running:
 
 ```
-Holmner6Axis
+sudo Holmner6Axis
 ```
--- RaspberryPi/Holmner6Axis/src/main.cpp
+++ RaspberryPi/Holmner6Axis/src/main.cpp
@@ -1,8 +1,43 @@
 
+// Pigpio includes.
+#include <pigpio.h>
+
+// Standard library includes.
 #include <iostream>
+#include <thread>
+
+void dummy(int pin)
+{
+    if (gpioInitialise() < 0)
+    {
+        std::cerr << "gpioInitialize failed\n";
+        gpioTerminate();
+        return;
+    }
+
+    if (gpioSetMode(pin, PI_OUTPUT) != 0)
+    {
+        std::cerr << "gpioSetMode failed\n";
+        gpioTerminate();
+        return;
+    }
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        gpioWrite(pin, 1);
+        gpioDelay(1000000);
+        gpioWrite(pin, 0);
+        gpioDelay(1000000);
+    }
+
+    gpioTerminate();
+}
 
 int main()
 {
-    std::cout << "Holmner6Axis\n";
+    std::cout << "Hello from Holmner6Axis\n";
+    std::thread th1([]{dummy(21);}); // Just for fun, run in worker thread.
+    th1.join();
     return 0;
 }
